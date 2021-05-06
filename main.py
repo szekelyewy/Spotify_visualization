55c912c13af807f100c25c821a6a0c08599c4e3b
Ewy <szekely.ewy@gmail.com>
2021-05-06 21:13:52 +0200

    Lasso regularization

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 
 # %% Adat beolvasás, tisztítás
-
 
 data = pd.read_csv('tracks.csv', encoding='utf-8')
 missing = data.isnull().sum()
 # print(missing) # -> csak a name oszlopban vannak Null értékek
-#data['name'].replace('', np.nan, inplace=True)
+# data['name'].replace('', np.nan, inplace=True)
 data.dropna(subset=['name'], inplace=True)
 data.drop(['id_artists', 'key', 'mode', 'time_signature'], 'columns', inplace=True)
-data['explicit'] = data['explicit'].astype('bool')
+#data['explicit'] = data['explicit'].astype('bool')
 data['release_date'] = data['release_date'].str[:4]
 # %% Bar chart
 df2 = data
@@ -35,14 +38,13 @@ plt.xticks(rotation=45, fontsize=16)
 plt.figure(figsize=(14, 7))
 plt.show()
 
-
-
 # %% Boxplot
 
 twenty = df2[df2['decade'] >= 2000]
 
 plt.figure(figsize=(12, 10))
-twenty.drop(['duration_ms', 'loudness', 'decade', 'popularity', 'tempo', 'explicit', 'instrumentalness'], 'columns', inplace=True)
+twenty.drop(['duration_ms', 'loudness', 'decade', 'popularity', 'tempo', 'explicit', 'instrumentalness'], 'columns',
+            inplace=True)
 bp = twenty.boxplot(notch=True, vert=True, patch_artist=True,
                     medianprops=dict(linestyle='solid', linewidth=4, color='y'),
                     whiskerprops=dict(linestyle='solid', linewidth=1, color='k'),
@@ -77,19 +79,65 @@ plt.legend(lines)
 plt.axis([1930, 2020, 0, 1])
 plt.show()
 
-
-
-
-
-
-
 # %% Heatmap
 twenty = df2[df2['decade'] >= 2000]
 corr = twenty.corr()
 plt.figure(figsize=(12, 11))
 sns.color_palette("icefire", as_cmap=True)
-sns.heatmap(corr, vmax=1, vmin=-1, center=0, linewidth=.5, square=True, annot=True, annot_kws={'size': 8}, fmt='.1f', cmap='icefire')
+sns.heatmap(corr, vmax=1, vmin=-1, center=0, linewidth=.5, square=True, annot=True, annot_kws={'size': 8}, fmt='.1f',
+            cmap='icefire')
 plt.xticks(rotation=45, fontsize=12)
 plt.yticks(rotation=45, fontsize=12)
 plt.title('Korrelációs mátrix', fontsize=25, pad=20)
 plt.show()
+
+# %% Train and test
+
+model_df = twenty[['explicit', 'danceability', 'speechiness']]
+X = model_df.drop('explicit', 'columns')
+y = model_df['explicit']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, shuffle=True)
+
+# %% Logistic Regression
+
+log_model = LogisticRegression()
+log_model.fit(X_train, y_train)
+y_pred = log_model.predict(X_test)
+print(np.sqrt(mean_squared_error(y_test, y_pred)))
+conf_y_test = y_test.to_numpy(copy=True)
+confusion = confusion_matrix(conf_y_test, y_pred)
+print(confusion)
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.imshow(confusion, cmap='BrBG_r')
+ax.grid(False)
+ax.xaxis.set(ticks=(0, 1), ticklabels=('Predicted 0s', 'Predicted 1s'))
+ax.yaxis.set(ticks=(0, 1), ticklabels=('Actual 0s', 'Actual 1s'))
+ax.set_ylim(1.5, -0.5)
+for i in range(2):
+    for j in range(2):
+        ax.text(j, i, confusion[i, j], ha='center', va='center', color='white')
+plt.title('Confusion Matrix of Predicted Explicity with Logistic Regression')
+plt.show()
+
+# %% Lasso Regularization
+
+lasso = LogisticRegression(penalty='l1', solver='liblinear')
+lasso.fit(X_train, y_train)
+y_pred = lasso.predict(X_test)
+print(np.sqrt(mean_squared_error(y_test, y_pred)))
+conf_y_test = y_test.to_numpy(copy=True)
+confusion = confusion_matrix(conf_y_test, y_pred)
+print(confusion)
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.imshow(confusion, cmap='BrBG_r')
+ax.grid(False)
+ax.xaxis.set(ticks=(0, 1), ticklabels=('Predicted 0s', 'Predicted 1s'))
+ax.yaxis.set(ticks=(0, 1), ticklabels=('Actual 0s', 'Actual 1s'))
+ax.set_ylim(1.5, -0.5)
+for i in range(2):
+    for j in range(2):
+        ax.text(j, i, confusion[i, j], ha='center', va='center', color='white', fontsize=13)
+plt.title('Confusion Matrix of Logistic Regression', fontsize=16)
+plt.show()
+
